6b22c8fee964d60b7d0ac7138bbba7dd2e5f848a
gustavo <gustavo.n.de.miranda@gmail.com>
2026-03-26 13:13:22 -0300

    Refine etapas section heading layout

---
+++ lp-cirurgiao-capilar/_nuxt/DFZEws9e.js
@@ -0,0 +1,1 @@
+import{f as e,g as a}from"./Dfbdj8Ax.js";const o=e(t=>{if(t.matched.length===0)throw a({statusCode:404,statusMessage:"Página não encontrada",fatal:!0})});export{o as default};
--- lp-cirurgiao-capilar/_nuxt/DmfyzxvN.js
+++
@@ -1,1 +0,0 @@
-import{f as e,g as a}from"./DW_pdu_g.js";const o=e(t=>{if(t.matched.length===0)throw a({statusCode:404,statusMessage:"Página não encontrada",fatal:!0})});export{o as default};
